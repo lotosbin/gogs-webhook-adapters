99c24cfeddaa287982021a5c776e8752d74ae53d
liubinbin <lotosbin@gmail.com>
2015-12-18 17:37:24 +0800

    fix

--- index.js
+++ index.js
@@ -24,14 +24,13 @@ app.use(parser.json());
 app.post('/gogs2talkai', function(request, response) {
     console.log(request.body);
     var talkaiurl = request.query.url;
-    //call talk.ai
     var data = {
   "authorName": request.body.sender.login,                          // 消息发送者的姓名，如果留空将显示为配置中的聚合标题
   "title": request.body.ref + ' ' + request.body.after,                    // 聚合消息标题
   "text": JSON.stringify(request.body.commits),                                     // 聚合消息正文
   "redirectUrl": request.body.compare_url,          // 跳转链接
   "imageUrl": request.body.sender.avatar_url             // 消息中可添加一张预览图片
-};
+    };
     talkaiwebhook(talkaiurl,data);
     response.type('text/xml');
     response.send('');
@@ -39,14 +38,13 @@ app.post('/gogs2talkai', function(request, response) {
 app.post('/github2talkai', function(request, response) {
     console.log(request.body);
     var talkaiurl = request.query.url;
-    //call talk.ai
     var data = {
   "authorName": request.body.sender.login,                          // 消息发送者的姓名，如果留空将显示为配置中的聚合标题
   "title": request.body.comment.body,
-  "text": JSON.stringify(request.body.comment),                                     // 聚合消息正文
+  "text": JSON.stringify(request.body),                                     // 聚合消息正文
   "redirectUrl": request.body.repository.html_url,          // 跳转链接
   "imageUrl": request.body.sender.avatar_url             // 消息中可添加一张预览图片
-};
+    };
     talkaiwebhook(talkaiurl,data);
     response.type('text/xml');
     response.send('');
